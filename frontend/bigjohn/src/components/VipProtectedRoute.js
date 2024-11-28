17c5f91cd7d81849086b6db3ca27844da2ef036a
ThomasSpare <t.spare.jkpg@gmail.com>
2024-11-28 17:50:59 +0100

    Changed VipProtectedRoute with isLoading prop

--- frontend/bigjohn/src/components/VipProtectedRoute.js
+++ frontend/bigjohn/src/components/VipProtectedRoute.js
@@ -3,8 +3,12 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Navigate } from "react-router-dom";
 
 const VipProtectedRoute = ({ children }) => {
-  const { isAuthenticated } = useAuth0();
-  console.log("VipProtectedRoute rendered", { isAuthenticated });
+  const { isAuthenticated, isLoading } = useAuth0();
+  console.log("VipProtectedRoute rendered", { isAuthenticated, isLoading });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   // Redirect to home page if not authenticated, not back to /vip
   return isAuthenticated ? children : <Navigate to="/" />;
